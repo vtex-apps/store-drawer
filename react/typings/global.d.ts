bccf6a12ca5f3009408c1ee5a260e2a3995cbfef
Lucas Bebber <lucasbbebber@gmail.com>
2019-04-26 17:09:07 -0300

    Adjust global.d.ts

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -1,28 +1,24 @@
 import { FunctionComponent } from 'react'
 
 declare global {
+  declare module '*'
 
   interface StorefrontComponent<P = {}> extends FunctionComponent<P> {
-    getSchema(props: P): object;
-	}
-	
-	enum Position{
-		left = 'left', 
-		right = 'right', 
-		up = 'up',
-		down = 'down',
-	}
-	
-	interface DrawerSchema {
-		actionIconId: string,
-		dismissIconId: string,
-		position : Position,
-		width: '100%' | 'auto',
-		height: '100%' | 'auto' | 'fullscreen',
-	}
+    getSchema(props: P): object
+  }
 
-	interface OverlaySchema {
-		opacity?: number,
-		color?: 'base' | 'base--inverted',
-	}
+  enum Position {
+    left = 'left',
+    right = 'right',
+    up = 'up',
+    down = 'down',
+  }
+
+  interface DrawerSchema {
+    actionIconId: string
+    dismissIconId: string
+    position: Position
+    width: '100%' | 'auto'
+    height: '100%' | 'auto' | 'fullscreen'
+  }
 }
