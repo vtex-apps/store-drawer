6eed94d91ade678b4800243abf7d2db8bc272813
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-25 18:18:05 -0300

    Fix scroll locking on iOS

--- react/Drawer.tsx
+++ react/Drawer.tsx
@@ -10,30 +10,94 @@ import Swipable from './Swipable'
 
 import styles from './drawer.css'
 
-const useMenuState = () => {
-  const [isMenuOpen, setIsOpen] = useState(false)
-  const [isMenuTransitioning, setIsTransitioning] = useState(false)
+// https://stackoverflow.com/a/3464890/5313009
+const getScrollPosition = () => {
+  const documentElement =
+    window && window.document && window.document.documentElement
+  if (!documentElement) {
+    return 0
+  }
+  return (
+    (window.pageYOffset || documentElement.scrollTop) -
+    (documentElement.clientTop || 0)
+  )
+}
+
+const useLockScroll = () => {
+  const [isLocked, setLocked] = useState(false)
+  type ScrollPosition = number | null
+  const [lockedScrollPosition, setLockedScrollPosition] = useState<
+    ScrollPosition
+  >(null)
 
   useEffect(() => {
     /** Locks scroll of the root HTML element if the
      * drawer menu is open
      */
+    const shouldLockScroll = isLocked
+
     const documentElement =
       window && window.document && window.document.documentElement
     if (documentElement) {
-      documentElement.style.overflow = isMenuOpen ? 'hidden' : 'auto'
+      documentElement.style.overflow = shouldLockScroll ? 'hidden' : 'auto'
+
+      /** iOS doesn't lock the scroll of the body by just setting overflow to hidden.
+       * It requires setting the position of the HTML element to fixed, which also
+       * resets the scroll position.
+       * This code is intended to record the scroll position and set it as
+       * the element's position, and revert it once the menu is closed.
+       */
+      const scrollPosition =
+        lockedScrollPosition == null
+          ? getScrollPosition()
+          : lockedScrollPosition
+
+      if (lockedScrollPosition == null && shouldLockScroll) {
+        setLockedScrollPosition(scrollPosition)
+      }
+
+      if (lockedScrollPosition != null && !shouldLockScroll) {
+        window && window.scrollTo(0, scrollPosition)
+        setLockedScrollPosition(null)
+      }
+
+      documentElement.style.position = shouldLockScroll ? 'fixed' : 'static'
+
+      documentElement.style.top = shouldLockScroll
+        ? `-${scrollPosition}px`
+        : 'auto'
+
+      documentElement.style.bottom = shouldLockScroll ? '0' : 'auto'
+      documentElement.style.left = shouldLockScroll ? '0' : 'auto'
+      documentElement.style.right = shouldLockScroll ? '0' : 'auto'
     }
 
     return () => {
       documentElement.style.overflow = 'auto'
+      documentElement.style.position = 'static'
+
+      documentElement.style.top = 'auto'
+      documentElement.style.bottom = 'auto'
+      documentElement.style.left = 'auto'
+      documentElement.style.right = 'auto'
     }
-  }, [isMenuOpen])
+  }, [isLocked]) // eslint-disable-line react-hooks/exhaustive-deps
+  // ☝️ no need to trigger this on lockedScrollPosition changes
+
+  return setLocked
+}
+
+const useMenuState = () => {
+  const [isMenuOpen, setIsOpen] = useState(false)
+  const [isMenuTransitioning, setIsTransitioning] = useState(false)
+  const setLockScroll = useLockScroll()
 
   let transitioningTimeout: number | null
 
   const setMenuOpen = (value: boolean) => {
     setIsOpen(value)
     setIsTransitioning(true)
+    setLockScroll(value)
 
     if (transitioningTimeout != null) {
       clearTimeout(transitioningTimeout)
