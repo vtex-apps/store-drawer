442b3a608457deebb70cb3de477d31776c62fcb2
victorhmp <victor2142@gmail.com>
2020-09-04 14:01:37 -0300

    Use typings for vtex.pixel-manager

--- react/typings/vtex.render-runtime.d.ts
+++ react/typings/vtex.render-runtime.d.ts
@@ -1,7 +1,4 @@
-/* eslint-disable import/order */
-
-import 'vtex.render-runtime'
-
 declare module 'vtex.render-runtime' {
   export const useChildBlock: (opts: { id: string }) => {} | null
+  export const ExtensionPoint: any
 }
